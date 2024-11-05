5048ff3d195133e77e0c5f74ef049caa420c61de
Phi Bao <tpbao122@gmail.com>
2024-11-05 03:37:08 -0800

    Surrounding Sound and Airplane

---
+++ Assets/Scripts/AirplaneMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class AirplaneMove : MonoBehaviour
+{
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform endPoint;
+
+    [Tooltip("Time to move between two point")]
+    [SerializeField] private float moveTime = 5f;
+
+    [Tooltip("Set the time that object will start moving after scene started in seconds")]
+    [SerializeField] private float startTime;
+
+    private float travelDist;
+
+    void Start()
+    {
+        travelDist = Vector3.Distance(startPoint.position, endPoint.position);
+    }
+
+    void Update()
+    {
+        MoveObject();
+    }
+
+    void MoveObject()
+    {
+        if (Time.time <= startTime) return;
+
+        float distCover = (Time.time - startTime) * moveTime;
+
+        float fracOfDist = distCover / travelDist;
+
+        transform.position = Vector3.Lerp(startPoint.position, endPoint.position, fracOfDist);
+    }
+}
---
+++ Assets/Scripts/SurroundSound.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SurroundSound : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> audioClips = new List<AudioClip>();
+
+    [SerializeField] private float minInterval = 5.0f;
+    [SerializeField] private float maxInterval = 15.0f;
+
+    [SerializeField] private bool randomLocation = true;
+    [SerializeField] private float maxDistance = 100;
+
+    private AudioSource audioSource;
+    private float timer = 0;
+
+    private int lastPlayedSound = -1;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        timer = Random.Range(minInterval, maxInterval);
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (audioSource.isPlaying) return;
+        
+        timer -= Time.deltaTime;
+        if(timer <= 0f)
+        {
+            if(randomLocation)
+            {
+                MoveToRandomLocation();
+            }
+
+            PlayRandomSound();
+        }
+    }
+
+    void PlayRandomSound()
+    {
+        int randIndex = Random.Range(0, audioClips.Count);
+
+        while(randIndex == lastPlayedSound) 
+        {
+            randIndex = Random.Range(0, audioClips.Count);
+        }
+
+        audioSource.clip = audioClips[randIndex];
+        audioSource.Play();
+        Debug.Log($"Play sound index: {randIndex}");
+
+        lastPlayedSound = randIndex;
+
+        timer = Random.Range(minInterval, maxInterval);
+    }
+
+    void MoveToRandomLocation()
+    {
+        float xPos = Random.Range(startPos.x - maxDistance, startPos.x + maxDistance);
+        float zPos = Random.Range(startPos.z - maxDistance, startPos.z + maxDistance);
+
+        transform.position = new Vector3(xPos, transform.position.y, zPos);
+    }
+}
