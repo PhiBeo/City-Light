a1621fef75a3cc4db532fdd5999b27a7a5a28829
LeahTaurisano <leahtaurisano@gmail.com>
2024-11-26 16:11:40 -0800

    Updated Windows Again

--- Assets/Scripts/WindowFlickering.cs
+++ Assets/Scripts/WindowFlickering.cs
@@ -10,34 +10,35 @@ public class WindowFlickering : MonoBehaviour
     [SerializeField] private float maxTimerDelay;
 
     private List<GameObject> windowPanes;
-    private List<Color> paneColorList;
+    private List<GameObject> windowPanesOff;
     private int randomIndex;
     private float timer = 0.0f;
-
-    private Color transparent = new Color(255, 255, 255, 0);
-    private Color opaque = new Color(0, 0, 0, 255);
-
-    bool firstPass = false;
-
-    private string emissionColor = "_EmissionColor";
 
     private void Start()
     {
         windowPanes = new List<GameObject>();
-        paneColorList = new List<Color>();
+        windowPanesOff = new List<GameObject>();
         foreach (Transform windowParent in windowPaneManager)
         {
-            foreach (Transform child in windowParent)
+            int i = 0;
+            foreach (Transform windowList in windowParent)
             {
-                windowPanes.Add(child.gameObject);
-                Material mat;
-                if (!HasRenderer(child.gameObject))
+                if (i == 0)
                 {
-                    continue;
+                    foreach (Transform window in windowList)
+                    {
+                        windowPanes.Add(window.gameObject);
+                    }
                 }
-                mat = child.GetComponent<Renderer>().material;
-                paneColorList.Add(mat.GetColor(emissionColor));
+                else
+                {
+                    foreach (Transform window in windowList)
+                    {
+                        windowPanesOff.Add(window.gameObject);
+                    }
+                }
             }
+            i = 1;
         }
     }
 
@@ -67,15 +68,8 @@ public class WindowFlickering : MonoBehaviour
     {
         randomIndex = Random.Range(0, windowPanes.Count);
         GameObject chosenWindow = windowPanes[randomIndex];
-        Material mat = chosenWindow.GetComponent<Renderer>().material;
-        if (mat.GetColor(emissionColor) == paneColorList[randomIndex])
-        {
-            mat.SetColor(emissionColor, Color.black);
-            mat.SetColor("_Color", opaque);
-            return;
-        }
-        mat.SetColor(emissionColor, paneColorList[randomIndex]);
-        mat.SetColor("_Color", transparent);
+        chosenWindow.SetActive(!chosenWindow.activeSelf);
+        windowPanesOff[randomIndex].SetActive(!chosenWindow.activeSelf);
 
     }
 
